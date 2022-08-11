a0eb0ede43096bed3023daf660133fafcc97ea7e
Anna <anyutka901990@mail.ru>
2022-08-11 11:30:38 +0300

    Zadanie 10

---
+++ Zalanie10/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите 3х значное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if ((n > 99) && (n<1000))
+{
+    Console.WriteLine(n.ToString()[1]);
+}
+else
+{
+    System.Console.WriteLine("Вы ввели не 3х значное число!");
+}
+
+
+
+
