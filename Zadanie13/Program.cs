0429bf6c6bf11b7ea18d8185f5d84d941106d10c
Anna <anyutka901990@mail.ru>
2022-08-11 11:31:10 +0300

    Zadanie 13

---
+++ Zadanie13/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n>99)
+{
+Console.WriteLine(n.ToString()[2]);
+}
+else
+{
+    System.Console.WriteLine("Третьей цифры нет!");
+}
+
+
+
+
+
+
+
+
